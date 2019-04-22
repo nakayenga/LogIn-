f4bd1703f2e298a7ec68f80e08ccffa1f2de3a3b
Catherine A. Nakayenga <nakayengacatherine@gmail.com>
2019-04-22 15:47:37 -0500

    Add files via upload

---
+++ LogIn.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace BHHC_ACCOUNTING
+{
+    partial class LogIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LogIn));
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.textBoxUsername = new System.Windows.Forms.TextBox();
+            this.buttonLogIn = new System.Windows.Forms.Button();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.labelEnterCredentials = new System.Windows.Forms.Label();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.BackColor = System.Drawing.Color.Transparent;
+            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.Location = new System.Drawing.Point(42, 137);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(71, 16);
+            this.labelUsername.TabIndex = 1;
+            this.labelUsername.Text = "Username";
+            this.labelUsername.Click += new System.EventHandler(this.labelUsername_Click);
+            // 
+            // textBoxUsername
+            // 
+            this.textBoxUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxUsername.Location = new System.Drawing.Point(180, 127);
+            this.textBoxUsername.Name = "textBoxUsername";
+            this.textBoxUsername.Size = new System.Drawing.Size(293, 26);
+            this.textBoxUsername.TabIndex = 3;
+            this.textBoxUsername.TextChanged += new System.EventHandler(this.textBoxUsername_TextChanged);
+            // 
+            // buttonLogIn
+            // 
+            this.buttonLogIn.BackColor = System.Drawing.Color.Gray;
+            this.buttonLogIn.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogIn.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonLogIn.Location = new System.Drawing.Point(398, 225);
+            this.buttonLogIn.Name = "buttonLogIn";
+            this.buttonLogIn.Size = new System.Drawing.Size(75, 31);
+            this.buttonLogIn.TabIndex = 5;
+            this.buttonLogIn.Text = "Log In";
+            this.buttonLogIn.UseVisualStyleBackColor = false;
+            this.buttonLogIn.Click += new System.EventHandler(this.buttonLogIn_Click);
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPassword.Location = new System.Drawing.Point(180, 177);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(293, 26);
+            this.textBoxPassword.TabIndex = 4;
+            this.textBoxPassword.TextChanged += new System.EventHandler(this.textBoxPassword_TextChanged);
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.BackColor = System.Drawing.Color.Transparent;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.Location = new System.Drawing.Point(42, 187);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(68, 16);
+            this.labelPassword.TabIndex = 2;
+            this.labelPassword.Text = "Password";
+            this.labelPassword.Click += new System.EventHandler(this.labelPassword_Click);
+            // 
+            // labelEnterCredentials
+            // 
+            this.labelEnterCredentials.AutoSize = true;
+            this.labelEnterCredentials.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelEnterCredentials.Location = new System.Drawing.Point(42, 68);
+            this.labelEnterCredentials.Name = "labelEnterCredentials";
+            this.labelEnterCredentials.Size = new System.Drawing.Size(181, 16);
+            this.labelEnterCredentials.TabIndex = 6;
+            this.labelEnterCredentials.Text = "Please enter credentials:";
+            this.labelEnterCredentials.Click += new System.EventHandler(this.labelEnterCredentials_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.BackColor = System.Drawing.Color.Gray;
+            this.buttonCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCancel.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.buttonCancel.Location = new System.Drawing.Point(291, 225);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 31);
+            this.buttonCancel.TabIndex = 7;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = false;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // LogIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(760, 290);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.labelEnterCredentials);
+            this.Controls.Add(this.buttonLogIn);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.textBoxUsername);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "LogIn";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "LogIn";
+            this.Load += new System.EventHandler(this.LogIn_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.TextBox textBoxUsername;
+        private System.Windows.Forms.Button buttonLogIn;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.Label labelEnterCredentials;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ LogIn.cs
@@ -0,0 +1,98 @@
+﻿/*
+========================================================================================================================================================
+AUTHOR
+Created By: Cathy Nakayenga
+Created On: 4/9/2019
+--------------------------------------------------------------------------------------------------------------------------------------------------------
+HISTORY:
+Date: Issue Resolved
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+DESCRIPTION:
+Show Quarterly Reports 
+==========================================================================================================================================================
+*/
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Windows;
+using System.Data.SqlClient;
+
+namespace BHHC_ACCOUNTING
+{
+    public partial class LogIn : Form
+    {
+        //Make sure that the password protection is * to hide the characters 
+        //Also make sure that the lenth of the password cannot exceed 20 characters
+        public LogIn()
+        {
+           
+            InitializeComponent();
+            textBoxPassword.PasswordChar = '*';
+            textBoxPassword.MaxLength = 20;
+        }
+
+        private void labelUsername_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LogIn_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        //Redirect us to the ExcelFilePreview when login button is clicked
+        private void buttonLogIn_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            PreviewExcelFile excelFile = new PreviewExcelFile();
+            excelFile.ShowDialog();
+
+            //remember to add:
+            //1. Server Explorer > Data Connection > User Database
+            //2. Add a connection string to the database in App.config
+            //3. Ask IT to create for me a view with the login details of finance/accounting that i can query
+
+            //SqlConnection con = new SqlConnection("put the connection string in here");
+            //SqlDataAdapter sda = new SqlDataAdapter("Select Role from Login where Username ='" + textBoxUsername.Text + "' and Password='" + textBox2Password.Text + "'", con);
+            //DataTable dt = new System.Data.DataTable();
+            //sda.Fill(dt);
+
+
+            
+        }
+
+        private void textBoxUsername_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        //close this page when the cancel button is clicked
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBoxPassword_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelEnterCredentials_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labelPassword_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
